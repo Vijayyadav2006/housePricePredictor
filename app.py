2b377b54778362dc94b224dcddf5364f9c4edf65
Vijayyadav2006 <vy945716@gmail.com>
2025-09-06 12:23:04 +0530

    adding codes

--- app.py
+++ app.py
@@ -4,16 +4,7 @@ import streamlit as st
 import pickle as pk
 
 
-import os
-
-model_path = 'House_prediction_model.pkl'
-if os.path.exists(model_path):
-    with open(model_path,'rb') as f:
-        model = pk.load(f)
-else:
-    st.error("Model file not found! Make sure 'House_prediction_model.pkl' is in the same folder as app.py")
-    st.stop()
-
+model = pk.load(open('House_prediction_model.pkl','rb'))
 
 st.header('Bangalore House Prices Predictor')
 data = pd.read_csv('Bengaluru_House_Data.csv')
